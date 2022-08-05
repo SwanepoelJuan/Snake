f6c707ab5bb09227a0f2077133e55529dc9d65ef
Juan <juan@urbec.co.za>
2022-08-06 01:11:32 +0200

    Constructor added. Public variables declared.

--- Snakepit.java
+++ Snakepit.java
@@ -1,3 +1,34 @@
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.*;
+import java.awt.*;
+
 public class Snakepit {
 
+    // GLOBAL VARIABLES
+    private static int CURRENT_FRUIT_ROW;
+    private static int CURRENT_FRUIT_COL;
+    private static int ROWS = 30;
+    private static int COLS = 12;
+
+    private boolean UP_MOVEMENT = false;
+    private boolean DOWN_MOVEMENT = false;
+    private boolean RIGHT_MOVEMENT = false;
+    private boolean LEFT_MOVEMENT = false;
+    private boolean inGame = false;
+
+    private JLabel labels[][] = new JLabel[ROWS][COLS];
+
+    private Timer timer = new Timer();
+    private JPanel panel = new JPanel();
+    private GridLayout gridLayout = new GridLayout(ROWS, COLS);
+    private GridBagConstraints gbc = new GridBagConstraints();
+
+    public Snakepit() {
+        // super("Snake!");
+    }
+
+    public static void main(String[] args) {
+        new Snakepit();
+    }
 }
